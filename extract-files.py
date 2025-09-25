94b1cf4284babbecd995c1e4b3bdebc1ad574e85
kenway214 <kenway214@outlook.com>
2025-09-25 21:55:37 +0200

    peridot: Fixup camera.xiaomi.so blob
    Change-Id: I9e3adbfb73bd05d181da5d9e03ac86dded6f5998
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -70,7 +70,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('xml=version', 'xml version'),
     (
         'odm/lib64/hw/camera.qcom.so',
-        'odm/lib64/hw/camera.xiaomi.so',
         'odm/lib64/hw/com.qti.chi.override.so',
         'odm/lib64/libchifeature2.so',
     ): blob_fixup()
@@ -78,9 +77,14 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed(
             'android.hardware.graphics.allocator-V1-ndk.so',
             'android.hardware.graphics.allocator-V2-ndk.so'
-        ),
+    ),
     'odm/lib64/hw/camera.xiaomi.so': blob_fixup()
-        .replace_needed('libui.so', 'libui-v34.so'),
+        .replace_needed('libui.so', 'libui-v34.so')
+        .add_needed('libprocessgroup_shim.so')
+        .replace_needed(
+            'android.hardware.graphics.allocator-V1-ndk.so',
+            'android.hardware.graphics.allocator-V2-ndk.so'
+    ),
     (
         'odm/lib64/camera/com.qti.actuator.peridot_aac_imx882_gt9764ber_wide_i_actuator.so',
         'odm/lib64/camera/com.qti.actuator.peridot_ofilm_imx882_aw86016csr_wide_ii_actuator.so',
